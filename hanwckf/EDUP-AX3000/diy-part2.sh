1c4597baad7e82f7b37909598f70ac51532422fb
Tony91590 <91764010+Tony91590@users.noreply.github.com>
2026-06-20 22:30:42 +0200

    Replace 'ImmortalWrt' with 'OpenWrt' in scripts

--- hanwckf/EDUP-AX3000/diy-part2.sh
+++ hanwckf/EDUP-AX3000/diy-part2.sh
@@ -4,7 +4,7 @@
 #
 # This is free software, licensed under the MIT License.
 # See /LICENSE for more information.
-#
+# ["FR"] = { 1, 2 },/["FR"] = { 1, 1 },
 # Custom for EDUP-AX3000 112m
 sed -i 's/ImmortalWrt/OpenWrt/g' package/base-files/files/bin/config_generate
 sed -i 's/ImmortalWrt/OpenWrt/g' package/mtk/applications/mtwifi-cfg/files/mtwifi.sh
