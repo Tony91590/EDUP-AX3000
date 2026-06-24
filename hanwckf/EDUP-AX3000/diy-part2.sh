afd3c129483aae04be6d74824e66ddf73fc4365d
Tony91590 <91764010+Tony91590@users.noreply.github.com>
2026-06-24 11:46:49 +0200

    Update diy-part2.sh

--- hanwckf/EDUP-AX3000/diy-part2.sh
+++ hanwckf/EDUP-AX3000/diy-part2.sh
@@ -39,45 +39,4 @@ cat > files/etc/uci-defaults/99-default-settings << 'EOF'
 exit 0
 EOF
 
-
-#!/bin/sh
-
-# Log des erreurs
-exec >/tmp/setup.log 2>&1
-
-# ===== WIFI CONFIG =====
-wlan_name="ImmortalWrt"
-wlan_password="12345678"
-
-# Sécurité minimale
-if [ -z "$wlan_name" ] || [ -z "$wlan_password" ]; then
-  echo "WiFi parameters missing, aborting"
-  exit 0
-fi
-
-# Déverrouille les radios (2.4 GHz + 5 GHz si présents)
-uci set wireless.@wifi-device[0].disabled='0'
-uci set wireless.@wifi-device[1].disabled='0'
-
-# ===== 2.4 GHz =====
-uci set wireless.@wifi-iface[0].disabled='0'
-uci set wireless.@wifi-iface[0].ssid="$wlan_name"
-uci set wireless.@wifi-iface[0].key="$wlan_password"
-uci set wireless.@wifi-iface[0].encryption='psk2'
-
-# ===== 5 GHz =====
-uci set wireless.@wifi-iface[1].disabled='0'
-uci set wireless.@wifi-iface[1].ssid="$wlan_name"
-uci set wireless.@wifi-iface[1].key="$wlan_password"
-uci set wireless.@wifi-iface[1].encryption='psk2'
-
-# Sauvegarde configuration WiFi
-uci commit wireless
-
-# Redémarre le WiFi
-wifi reload
-
-echo "WiFi configured successfully"
-exit 0
-
 chmod +x files/etc/uci-defaults/99-default-settings
