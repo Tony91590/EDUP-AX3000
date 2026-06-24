c95cd134a6cb9689d9bda4afa05cc73380cd1302
Tony91590 <91764010+Tony91590@users.noreply.github.com>
2026-06-24 10:09:01 +0200

    Update diy-part2.sh

--- hanwckf/EDUP-AX3000/diy-part2.sh
+++ hanwckf/EDUP-AX3000/diy-part2.sh
@@ -21,8 +21,21 @@ mkdir -p files/etc/uci-defaults
 
 cat > files/etc/uci-defaults/99-default-settings << 'EOF'
 #!/bin/sh
-uci set luci.main.mediaurlbase='/luci-static/argon'
-uci commit luci
+
+  # Radio 0 (2.4 GHz)
+  uci set wireless.@wifi-device[0].disabled='0'
+  uci set wireless.@wifi-iface[0].disabled='0'
+  uci set wireless.@wifi-iface[0].encryption='none'
+  uci set wireless.@wifi-iface[0].ssid="OpenWrt"
+
+  # Radio 1 (5 GHz)
+  uci set wireless.@wifi-device[1].disabled='0'
+  uci set wireless.@wifi-iface[1].disabled='0'
+  uci set wireless.@wifi-iface[1].encryption='none'
+  uci set wireless.@wifi-iface[1].ssid="OpenWrt_5G"
+
+  uci commit wireless
+  
 exit 0
 EOF
 
