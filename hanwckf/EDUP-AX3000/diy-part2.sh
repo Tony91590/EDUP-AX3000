da842c0ec824d743b87e2ea5020c0c287b563874
Tony91590 <91764010+Tony91590@users.noreply.github.com>
2026-06-24 10:05:29 +0200

    Update diy-part2.sh

--- hanwckf/EDUP-AX3000/diy-part2.sh
+++ hanwckf/EDUP-AX3000/diy-part2.sh
@@ -8,31 +8,6 @@
 # Custom for EDUP-AX3000 112m
 #
 
-sed -i 's/ImmortalWrt/OpenWrt/g' package/base-files/files/bin/config_generate
-sed -i 's/pool.ntp.org/3.openwrt.pool.ntp.org/g' package/base-files/files/bin/config_generate
-sed -i 's/time1.apple.com/0.openwrt.pool.ntp.org/g' package/base-files/files/bin/config_generate
-sed -i 's/time1.google.com/1.openwrt.pool.ntp.org/g' package/base-files/files/bin/config_generate
-sed -i 's/time.cloudflare.com/2.openwrt.pool.ntp.org/g' package/base-files/files/bin/config_generate
-
-sed -i 's/default-settings-chn/default-settings/g' include/target.mk
-
-sed -i 's/ImmortalWrt/OpenWrt/g' include/version.mk
-sed -i 's,https://immortalwrt.org/,https://openwrt.org/,g' include/version.mk
-sed -i 's,https://github.com/immortalwrt/immortalwrt/issues,https://bugs.openwrt.org/,g' include/version.mk
-sed -i 's,https://github.com/immortalwrt/immortalwrt/discussions,https://forum.openwrt.org/,g' include/version.mk
-sed -i 's,https://downloads.immortalwrt.org/releases/21.02-SNAPSHOT,https://downloads.openwrt.org/releases/21.02-SNAPSHOT,g' include/version.mk
-
-cat > package/base-files/files/etc/banner << 'EOF'
-  _______                     ________        __
- |       |.-----.-----.-----.|  |  |  |.----.|  |_
- |   -   ||  _  |  -__|     ||  |  |  ||   _||   _|
- |_______||   __|_____|__|__||________||__|  |____|
-          |__| W I R E L E S S   F R E E D O M
- -----------------------------------------------------
- %D %V, %C
- -----------------------------------------------------
-EOF
-
 # ===== Argon Theme =====
 
 rm -rf package/luci-theme-argon
