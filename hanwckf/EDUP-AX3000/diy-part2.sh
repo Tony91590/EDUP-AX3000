214c4cbffe8d9a89c9e6c4df6dcf9b5c6df217f8
Tony91590 <91764010+Tony91590@users.noreply.github.com>
2026-06-25 17:23:52 +0200

    Update diy-part2.sh

--- hanwckf/EDUP-AX3000/diy-part2.sh
+++ hanwckf/EDUP-AX3000/diy-part2.sh
@@ -6,6 +6,9 @@
 # See /LICENSE for more information.
 # 
 # Custom for EDUP-AX3000 112m
+
+sed -i 's/model = "CLT R30B1"/model = "EP-2980"/g' target/linux/mediatek/files-5.4/arch/arm64/boot/dts/mediatek/mt7981-clt-r30b1-base.dtsi
 sed -i 's/default-settings-chn/default-settings/g' include/target.mk
 sed -i '/\["FR"\]/s/{ 1, 2 }/{ 1, 1 }/' package/mtk/applications/mtwifi-cfg/files/mtwifi-cfg/mtwifi_defs.lua
 grep -R '\["FR"\]' package/mtk/applications/mtwifi-cfg/files/mtwifi-cfg/mtwifi_defs.lua
+grep -R 'model = "EP-2980"' -n target/linux/mediatek/files-5.4/arch/arm64/boot/dts/mediatek/mt7981-clt-r30b1-base.dtsi
