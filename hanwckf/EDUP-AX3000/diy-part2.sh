f9edd8d82429bc6254f2ab446aa788eab9e6770c
Tony91590 <91764010+Tony91590@users.noreply.github.com>
2026-06-28 00:20:21 +0200

    Uncomment sed commands in diy-part2.sh

--- hanwckf/EDUP-AX3000/diy-part2.sh
+++ hanwckf/EDUP-AX3000/diy-part2.sh
@@ -6,17 +6,17 @@
 # See /LICENSE for more information.
 # 
 # Custom for EDUP-AX3000 112m
-sed -i '/"FR"/s/\[ 1, 2 \]/[ 1, 1 ]/' package/mtk/applications/mtwifi-cfg-ucode/files/usr/share/schema/mtwifi/dat-defs.json
+#sed -i '/"FR"/s/\[ 1, 2 \]/[ 1, 1 ]/' package/mtk/applications/mtwifi-cfg-ucode/files/usr/share/schema/mtwifi/dat-defs.json
 #sed -i 's/ImmortalWrt/OpenWrt/g' package/base-files/files/bin/config_generate
 #sed -i 's/ImmortalWrt/OpenWrt/g' package/mtk/applications/mtwifi-cfg/files/mtwifi.sh
 #sed -i 's/pool.ntp.org/3.openwrt.pool.ntp.org/g' package/base-files/files/bin/config_generate
 #sed -i 's/time1.apple.com/0.openwrt.pool.ntp.org/g' package/base-files/files/bin/config_generate
 #sed -i 's/time1.google.com/1.openwrt.pool.ntp.org/g' package/base-files/files/bin/config_generate
 #sed -i 's/time.cloudflare.com/2.openwrt.pool.ntp.org/g' package/base-files/files/bin/config_generate
-#sed -i 's/default-settings-chn/default-settings/g' include/target.mk
+sed -i 's/default-settings-chn/default-settings/g' include/target.mk
 #sed -i 's/ImmortalWrt/OpenWrt/g' include/version.mk
 #sed -i 's,https://immortalwrt.org/,https://openwrt.org/,g' include/version.mk
 #sed -i 's,https://github.com/immortalwrt/immortalwrt/issues,https://bugs.openwrt.org/,g' include/version.mk
 #sed -i 's,https://github.com/immortalwrt/immortalwrt/discussions,https://forum.openwrt.org/,g' include/version.mk
 #sed -i 's,https://downloads.immortalwrt.org/releases/21.02-SNAPSHOT,https://downloads.openwrt.org/releases/21.02.7,g' include/version.mk
-grep -n '"FR"' package/mtk/applications/mtwifi-cfg-ucode/files/usr/share/schema/mtwifi/dat-defs.json
+#grep -n '"FR"' package/mtk/applications/mtwifi-cfg-ucode/files/usr/share/schema/mtwifi/dat-defs.json
